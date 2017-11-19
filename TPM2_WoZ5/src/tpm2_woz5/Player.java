cfd1ae84b9cfbabb9f389911abb598f46d45f023
wanarchit <wanarchit@gmail.com>
2017-11-19 12:44:04 +0100

    player class

---
+++ TPM2_WoZ5/src/tpm2_woz5/Player.java
@@ -0,0 +1,53 @@
+package tpm2_woz5;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.ArrayList;
+import tpm2_woz2.Weapon;
+
+
+/**
+ *
+ * @author Paul
+ */
+public class Player {
+    private String aliasP;
+    private ArrayList<Weapon> weaponP;
+    private int strengthP;
+
+    public Player(String pseudoJ, ArrayList<Weapon> armeJ, int forceJ) {
+        this.aliasP = pseudoJ;
+        this.weaponP = armeJ;
+        this.strengthP = forceJ;
+    }
+
+    public String getAliasP() {
+        return aliasP;
+    }
+
+    public void setAliasP(String aliasP) {
+        this.aliasP = aliasP;
+    }
+
+    public ArrayList<Weapon> getWeaponP() {
+        return weaponP;
+    }
+
+    public void setWeaponP(ArrayList<Weapon> weaponP) {
+        this.weaponP = weaponP;
+    }
+
+    public int getStrengthP() {
+        return strengthP;
+    }
+
+    public void setStrengthP(int strengthP) {
+        this.strengthP = strengthP;
+    }
+    
+}
